402ac6fc8a10ce002552305b78a49d06c253f767
rie <skyhigh_rainbow@hotmail.com>
2024-08-30 21:01:18 +0900

    no message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,22 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+// use App\Models\Category;
 
 class PostController extends Controller
 {
     private $post;
+    // private $category;
+
+    // public function __construct(Post $post, )
+    // {
+    //     $this->post = $post;
+    // //     $this->category = $category;
+    // }
+
+
+    //create() - view Create Post Page
+    public function showCreate()
+    {
+
+        return view('posts.create');
+    }
 
     # index() - view the post index page
-    public function index()
-    {
-        return view('users.posts.index');
-    }
+    // public function index()
+    // {
+    //     return view('users.posts.index');
+    // }
 
-    // show() - view Show Post Page
-    public function show($id)
-    {
-        $post = $this->post->findOrFail($id);
+    // // show() - view Show Post Page
+    // public function show($id)
+    // {
+    //     $post = $this->post->findOrFail($id);
 
-        return view('users.posts.show');
-    }
+    //     return view('users.posts.show');
+    // }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,28 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/search', function () {
-    return view('search');
-});
-Route::get('/users', function () {
-    return view('users.community.index');
-});
+// Route::get('/search', function () {
+//     return view('search');
+// });
+// Route::get('/users', function () {
+//     return view('users.community.index');
+// });
+
+// // Route::get('/post/create', function () {
+// //     return view('users.posts.create');
+// });
+
+// Route::get('/', function () {
+//     return view('post.create');
+// });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/post', [PostController::class, 'index'])->name('users.posts.index');
+//Post
+Route::get('/post/index', [PostController::class, 'index'])->name('users.posts.index');
 Route::get('/post/{id}/show', [PostController::class, 'show'])->name('users.posts.show');
+Route::get('/post/create', [PostController::class, 'create'])->name('users.posts.create');
 
-Route::get('search', [HomeController::class,'search'])->name('search');
+// Route::get('/search', [HomeController::class,'search'])->name('search');
