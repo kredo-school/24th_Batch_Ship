8282185b6d7db25d7182a7e2fdb97b1f77f32ffc
村瀬日菜 <>
2024-08-30 12:03:23 +0900

    [UI/community-create]
    Additions:
    -Added create.blade.php file
    -Added routes for community create to web.php
    -Added a couple of function to CommunityController

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,18 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('users.community.index');
-});
-
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// Route::get('/post', [PostController::class, 'index'])->name('users.posts.index');
-// Route::get('/post/{id}/show', [PostController::class, 'show'])->name('users.posts.show');
+Route::get('/', function () {
+    return view('users.community.create');
+
+    # Community
+Route::get('/community/create',[CommunityController::class,'create'])->name('community.create');
+Route::post('/community/store',[CommunityController::class,'store'])->name('community.store');
+});
+
+
+
+
+
