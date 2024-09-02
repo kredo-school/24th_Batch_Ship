db20a8ce096b10c22c127b48c06fed121f84d0ca
yuki <>
2024-09-02 20:14:56 +0900

    Resolved conflicts

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('users.posts.index');
