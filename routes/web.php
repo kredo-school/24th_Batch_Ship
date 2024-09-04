73ba1904475f144dce02d0c698caa2a5ed3d7cbd
rie <skyhigh_rainbow@hotmail.com>
2024-09-04 21:52:20 +0900

    no message

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\HomeController;
+
 use App\Http\Controllers\CommunityController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\EventController;
@@ -12,37 +13,21 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function(){
 
+    Route::get('/search', [HomeController::class, 'search'])->name('search');
 
-    Route::get('/search', function () {
-        return view('search');
-    });
+    //Profile
+    Route::get('/', [ProfileController::class, 'index'])->name('users.profile.index');
+    Route::get('/profile/create', [ProfileController::class, 'create'])->name('users.profile.create');
 
-    Route::get('/post/create', function () {
-        return view('users.posts.create');
-    });
+    //Post
+    Route::get('/post/index', [PostController::class, 'index'])->name('users.posts.index');
+    Route::get('/post/{id}/show', [PostController::class, 'show'])->name('users.posts.show');
+    Route::get('/post/create', [PostController::class, 'create'])->name('users.posts.create');
 
-    // Route::get('/users', function () {
-        //     return view('users.community.index');
-        // });
-
-
-        // Route::get('/', function () {
-        //     return view('auth.login');
-        // });
-
-
-
-
-        //Post
-        Route::get('/post/index', [PostController::class, 'index'])->name('users.posts.index');
-        Route::get('/post/{id}/show', [PostController::class, 'show'])->name('users.posts.show');
-        Route::get('/post/create', [PostController::class, 'create'])->name('users.posts.create');
-
-        Route::get('/search', [HomeController::class, 'search'])->name('search');
-
-        //Profile
-        Route::get('/', [ProfileController::class, 'index'])->name('users.profile.index');
-        Route::get('/profile/create', [ProfileController::class, 'create'])->name('users.profile.create');
+    # Community
+    Route::get('community/index', [CommunityController::class,'index'])->name('community.index');
+    Route::get('/community/create',[CommunityController::class,'create'])->name('community.create');
+    Route::post('/community/store',[CommunityController::class,'store'])->name('community.store');
 
     # Event
     Route::get('/event/create', [EventController::class, 'create'])->name('event.create');
@@ -51,6 +36,5 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/event/edit', [EventController::class, 'edit'])->name('event.edit');
     Route::patch('/event/update', [EventController::class, 'update'])->name('event.update');
     Route::delete('/event/destroy', [EventController::class, 'destroy'])->name('event.destroy');
-
 
 });
