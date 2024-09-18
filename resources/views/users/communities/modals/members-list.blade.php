b52492f66f42ef71e3c8ea5dbcf6f4e0af964178
Chisaki Sera <>
2024-09-18 21:31:44 +0900

    [UI/BE community-show] - Updated Community show page - Reflected the relationship between Community and Event

--- resources/views/users/communities/modals/members-list.blade.php
+++ resources/views/users/communities/modals/members-list.blade.php
@@ -1,50 +1,48 @@
-<div class="modal fade" id="community_members{{-- $community->user_id? --}}">
+<div class="modal fade" id="community-members-{{ $community->id }}">
   <div class="modal-dialog">
 
     {{-- visible part --}}
     <div class="modal-content border border-3 border-turquoise"> 
-      <div class="modal-header">
+      <div class="modal-header text-center border-0 d-block pb-0">
         <div class="container">
           <h6 class="text-center pb-2">Someone who is interested in this community</h6>
-          <div class="d-flex justify-content-center">
-            <div class="fs-6 pt-1 px-2">sort by</div>
-            <div class="mx-0"><button class="btn bg-turquoise text-white py-1 px-5"> interest %</button></div>        
-            <p class="pt-1 px-2">or</p>
-            <div><button class="btn bg-turquoise text-white py-1 px-3"> date(newest first)</button></div>
-            <hr>
-         </div>       
-       </div> {{-- end of container --}}
-     </div>  {{-- end of modal-header --}}
+          <div class="d-flex justify-content-center align-items-center mt-3">
+            <p class="mx-2 px-2 mb-0">sort by</p>
+            <button class="btn bg-turquoise text-white"> interest %</button>
+            <p class="mx-2 px-2 mb-0">or</p>
+            <button class="btn bg-turquoise text-white"> date(newest first)</button>
+          </div> 
+        </div> {{-- end of container --}}
+      </div>  {{-- end of modal-header --}}
 
-      <div class="modal-body">
-         {{-- @foreach ($community as $members)} --}}
+      <div class="modal-body border-0 my-1 pt-0" style="max-height: 400px; overflow-y: auto;">
+        @foreach ($all_members as $member)
+          <hr>
           <div class="row mb-1">
             <div class="col-2">
-                {{-- @if ($user->interest%? ) --}}
-                <p class="text-end" >{{-- $user->interest% --}} %</p>
-                {{-- @else --}}                  
-                {{-- @endif --}}
+              {{-- @if ($user->interest%? ) --}}
+              <p class="text-end" >{{-- $user->interest% --}} %</p>
+              {{-- @else --}}                  
+              {{-- @endif --}}
             </div>
-            <div class="col-2">
-              <a href="{{-- route('profile.index', $user->id ) --}}" class="me-auto">
-                {{-- @if ($user->avatar) --}}
-                    <img src="{{-- $user->avatar --}}" alt="" class="rounded-circle avatar-sm">
-                {{-- @else --}}
+            <div class="col-2 d-flex align-items-center">
+              <a href="{{ route('users.profile.specificProfile', $member->user_id) }}" class="me-3">
+                @if ($member->user->avatar)
+                  <img src="{{ $member->user->avatar }}" alt="{{ $member->user->username }}" class="rounded-circle avatar-sm">
+                @else
                   <i class="fa-solid fa-circle-user text-dark icon-sm"></i>
-                {{--@endif --}}
+                @endif
               </a>
-            </div>
-            <div class="col">
-              <h6 class="">username</h6>
+              <a href="{{ route('users.profile.specificProfile', $member->user_id) }}" class="text-decoration-none text-black ms-0">
+                <h6 class="">{{ $member->user->username }}</h6>
+              </a>
             </div>
             <div class="col">
               <p class="text-muted fw-light mb-1 ms-auto">{{-- date('M-d--Y', strtotime()) --}} 15-Aug-2024</p>
             </div>
-
           </div>
-               {{-- @endforeach --}}
-               {{-- @endif --}}
-          <hr class="my-2">
+        @endforeach
+        <hr class="my-2">
       </div> {{-- end of modal-body --}}
 
     </div>
