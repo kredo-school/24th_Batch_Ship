d729d74e50a766bc0eff1431ad7810ab24fe221d
村瀬日菜 <>
2024-09-19 19:43:11 +0900

    no message

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use App\Models\Post;
 use App\Models\CategoryPost;
 use App\Models\Category;
@@ -36,7 +37,7 @@ class PostController extends Controller
         $all_posts = $this->getAllPosts();
         return view('users.posts.index')
             ->with('all_posts', $all_posts);
-        // return view('users.posts.index');
+        
     }
 
     private function getAllPosts()
@@ -54,12 +55,6 @@ class PostController extends Controller
     }
 
 
-    // # show() - view Show Post Page
-    // public function show()
-    // {
-    //     return view('users.posts.show');
-    // }
-
     # show() - view Show Post Page
     public function show($id)
     {
@@ -72,14 +67,15 @@ class PostController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'description'   => 'required|min:1|max:1500',
-            'image'      => 'required|mimes:jpg,jpeg,png,gif|max:1048',
+            'description'   => 'max:1500',
+            'image'      => 'mimes:jpg,jpeg,png,gif|max:1048',
             'category'      => 'required|array|between:1,3'
         ]);
 
         # Save the post
         $this->post->user_id        = Auth::user()->id;
-        $this->post->image          = 'data:image/' . $request->image->extension() . ';base64,' . base64_encode(file_get_contents($request->image));
+        if($request->image){
+        $this->post->image          = 'data:image/' . $request->image->extension() . ';base64,' . base64_encode(file_get_contents($request->image));}
         $this->post->description    = $request->description;
         $this->post->save();
 
@@ -111,57 +107,57 @@ class PostController extends Controller
 
         # GET all the category IDs of this POST. Then save it in a ARRAY
         $selected_categories = [];
-        foreach($post->category as $category){
-            $selected_categories[] = $category->category_id;
+        foreach($post->categoryPost as $category_post){
+            $selected_categories[] = $category_post->category_id;
         }
 
-        // return view('users.posts.edit')
-        //         ->with('post', $post)
-        //         ->with('all_categories', $all_categories)
-        //         ->with('selected_categories', $selected_categories);
-    //}
-
-    // public function update(Request $request, $id)
-    // {
-    //     # 1. VALIDATE THE DATA FROM THE FORM
-    //     $request->validate([
-    //         'category'      => 'required|array|between:1,3',
-    //         'description'   => 'required|min:1|max:1000',
-    //         'image'         => 'mimes:jpg,jpeg,png,gif|max:1048'
-    //     ]);
-
-    //     # 2. UPDATE THE POST
-    //     $post                 = $this->post->findOrFail($id);
-    //     $post->description    = $request->description;
-
-    //     #IF there is a new image
-    //     if($request->image){
-    //         $post->image      = 'data:image/' . $request->image->extension() . ';base64,' . base64_encode(file_get_contents($request->image));
-    //     }
-
-    //    $post->save();
-
-    //    # 3. DELETE ALL RECORDS from the category_post table related to this POST
-    //    $post->categoryPost()->delete();
-
-    //    # 4. SAVE the new categories to the category_post table
-    //    foreach($request->category as $category_id){
-    //         $category_post[]  =  ['category_id' => $category_id];
-    //    }
-    //    $post->categoryPost()->createMany($category_post);
-
-    //    # 5. REDIRECT to Shoe Post page
-    //    return redirect()->route('post.show', $id);
+        return view('users.posts.edit')
+                ->with('post', $post)
+                ->with('all_categories', $all_categories)
+                ->with('selected_categories', $selected_categories);
     }
 
-    // destroy() - delete the post
-    // public function destroy($id)
-    // {
-    //     $post = $this->post->findOrFail($id);
-    //     $post->forceDelete();
+    public function update(Request $request, $id)
+    {
+        # 1. VALIDATE THE DATA FROM THE FORM
+        $request->validate([
+            'category'      => 'required|array|between:1,3',
+            'description'   => 'required|min:1|max:1000',
+            'image'         => 'mimes:jpg,jpeg,png,gif|max:1048'
+        ]);
 
-    //     return redirect()->route('index');
-    // }
+        # 2. UPDATE THE POST
+        $post                 = $this->post->findOrFail($id);
+        $post->description    = $request->description;
+
+        #IF there is a new image
+        if($request->image){
+            $post->image      = 'data:image/' . $request->image->extension() . ';base64,' . base64_encode(file_get_contents($request->image));
+        }
+
+       $post->save();
+
+       # 3. DELETE ALL RECORDS from the category_post table related to this POST
+       $post->categoryPost()->delete();
+
+       # 4. SAVE the new categories to the category_post table
+       foreach($request->category as $category_id){
+            $category_post[]  =  ['category_id' => $category_id];
+       }
+       $post->categoryPost()->createMany($category_post);
+
+       # 5. REDIRECT to Shoe Post page
+       return redirect()->route('post.show', $id);
+    }
+
+    //destroy() - delete the post
+    public function destroy($id)
+    {
+        $post = $this->post->findOrFail($id);
+        $post->forceDelete();
+
+        return redirect()->route('index');
+    }
 
 
 
