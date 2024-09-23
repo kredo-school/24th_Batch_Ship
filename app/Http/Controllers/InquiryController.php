3ffb350341ad7d635b37b716d759b5514d304a42
Chisaki Sera <>
2024-09-24 00:18:01 +0900

    [UI/BE Inquiry] - Updated Inquiry UI form and method

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -19,4 +19,30 @@ class InquiryController extends Controller
     {
         return view('users.inquiries.create');
     }
+
+    public function store(Request $request)
+    {
+        # 1. Validate the form data
+        $request->validate([
+            'subject'  => 'required|string|max:255',
+            'message'  => 'required|string'
+        ]);
+    
+        # 2. Save the inquiry
+        $this->inquiry->user_id = Auth::user()->id;
+        $this->inquiry->subject = $request->subject;
+        $this->inquiry->message = $request->message;
+        $this->inquiry->save();
+    
+        # 3. Redirect to Show Inquiry page
+        return redirect()->route('inquiry.submitted', $this->inquiry->id);
+    }
+
+    public function submitted($id)
+    {
+        // $id - ID of the inquiry
+        $inquiry = $this->inquiry->findOrFail($id);
+
+        return view('users.inquiries.submitted', compact('inquiry'));
+    }
 }
