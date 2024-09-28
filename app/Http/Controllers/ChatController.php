629cdcbdcda095247ba20b3c4170ffba14a08a67
yuki <>
2024-09-28 17:40:18 +0900

    [backend/chat]
    Changes:
    - modified index method in ChatController
    - added chat icon on nav bar to access chat page
    - edited chat.index & profile.header & route
    - created css file & js file for chat

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -20,37 +20,11 @@ class ChatController extends Controller
         $this->user = $user;
     }
 
-    public function index($profile_id){
-        # GET ALL CHATS
-        $all_chats = Chat::where('sender_id', Auth::id())
-                        ->orWhere('recipient_id', Auth::id())
-                        ->get();
-        
-        # GET ALL MESSAGES
-        $sender_id = Auth::id();
-        $recipient_id = $profile_id;
-
-        // identify chat
-        $chats = Chat::where(function($query) use ($sender_id, $recipient_id) {
-            $query->where('sender_id', $sender_id)
-                  ->where('recipient_id', $recipient_id)
-                  ->with('messages');
-        })
-        ->orWhere(function($query) use ($sender_id, $recipient_id) {
-            $query->where('sender_id', $recipient_id)
-                  ->where('recipient_id', $sender_id)
-                  ->with('messages');
-        })
-        ->get();
-
-        $all_messages = $chats->flatMap(function($chat){
-            return $chat->messages;
-        });
-
-        return view('users.chats.index', compact('all_chats', 'profile_id', 'all_messages'));
+    public function index(){
+        return $this->getAllChat(Auth::user()->id);
     }
 
-    # STORE MESSAGES
+    # to store messages
     public function store(Request $request, $profile_id){
         $sender_id = Auth::id();
         $recipient_id = $profile_id;
@@ -83,9 +57,9 @@ class ChatController extends Controller
             $this->chatmessage->user_id = Auth::user()->id;
             $this->chatmessage->chat_id = $chat->id;
             $this->chatmessage->save();
-            // return redirect()->route('chat.index', ['chat' => $chat->id]);
 
         } else {
+
             // No conversation exists yet
             $this->chat->sender_id = Auth::user()->id;
             $this->chat->recipient_id = $profile_id;
@@ -105,13 +79,56 @@ class ChatController extends Controller
             $this->chatmessage->user_id = Auth::user()->id;
             $this->chatmessage->chat_id = $this->chat->id;
             $this->chatmessage->save();
-            // return redirect()->route('chat.index');
         }
 
         return redirect()->back();
     }
 
-    public function identifyChat(){
+    # process to get chats and messages
+    public function getAllChat($profile_id){
+        // get all chats
+        $all_chats = Chat::where('sender_id', Auth::id())
+                        ->orWhere('recipient_id', Auth::id())
+                        ->get();
+        
+        // get all messages
+        $sender_id = Auth::id();
+        $recipient_id = $profile_id;
 
+        // identify chat
+        $chats = Chat::where(function($query) use ($sender_id, $recipient_id) {
+            $query->where('sender_id', $sender_id)
+                  ->where('recipient_id', $recipient_id)
+                  ->with('messages');
+        })
+        ->orWhere(function($query) use ($sender_id, $recipient_id) {
+            $query->where('sender_id', $recipient_id)
+                  ->where('recipient_id', $sender_id)
+                  ->with('messages');
+        })
+        ->get();
+
+        $all_messages = $chats->flatMap(function($chat){
+            return $chat->messages;
+        });
+
+        // $latest_massage = $this->getLatestMessage();
+
+        return view('users.chats.index', compact('all_chats', 'profile_id', 'all_messages'));
     }
+
+    // public function getLatestMessage(){
+    //     $sender_id = Auth::id();
+
+    //     $chat = Chat::where(function($query) use ($sender_id) {
+    //         $query->where('sender_id', $sender_id)
+    //               ->orWhere('recipient_id', $sender_id);
+    //     })
+    //     ->with(['messages' => function($query){
+    //         $query->orderBy('created_at', 'desc')->limit(1);
+    //     }])
+    //     ->get();
+
+    //     return view('users.chats.index', compact('chat'));
+    // }
 }
