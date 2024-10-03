5f1e7fb3a7b31fc665f7ed1e3e2d88992fdacd7f
HanaA520 <haniel236t@gmail.com>
2024-10-03 19:56:19 +0900

    Edit - BoardCommentController, show.blade.php and web.php to fix the error of sorting comment.

--- app/Http/Controllers/BoardCommentController.php
+++ app/Http/Controllers/BoardCommentController.php
@@ -22,20 +22,20 @@ class BoardCommentController extends Controller
         $this->user = $user;
     }
 
-    public function store(Request $request, $community_id)
+    public function store(Request $request)
     {
         $request->validate([
-            'comment_body' . $community_id => 'required|max:200'
+            'comment_body' => 'required|max:200'
         ], [
-            'comment_body' . $community_id . '.required' => 'You cannot submit an empty comment.',
-            'comment_body' . $community_id . '.max' => 'The comment must not have more than 200 characters.'
+            'comment_body.required' => 'You cannot submit an empty comment.',
+            'comment_body.max' => 'The comment must not have more than 200 characters.'
         ],[
             'image'      => 'nullable|mimes:jpg,jpeg,png,gif|max:1048',
         ]);
 
-        $this->boardcomment->body    = $request->input('comment_body' . $community_id);
+        $this->boardcomment->body    = $request->input('comment_body');
         $this->boardcomment->user_id = Auth::user()->id;
-        $this->boardcomment->community_id = $community_id;
+        $this->boardcomment->community_id = $request->input('community_id');
 
         if($request->image){
             $this->boardcomment->image          = 'data:image/' . $request->image->extension() . ';base64,' . base64_encode(file_get_contents($request->image));
